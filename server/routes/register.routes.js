fbcbe995c64e1067c4f0811ff1e840ff125e9076
PhiB64 <philippebarbosa64@gmail.com>
2025-09-03 16:22:58 +0200

    feat: ajout route verify

--- server/routes/register.routes.js
+++ server/routes/register.routes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const { createUser } = require("../controllers/users.controller.js");
 
-// POST /api/register
-
 router.post("/", createUser);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ const app = express();
 require("dotenv").config();
 const connectDB = require("./db/config.db.js");
 const userRoutes = require("./routes/register.routes.js");
+const verifyRoutes = require("./routes/verify.routes.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 app.use("/register", userRoutes);
+
+app.use("/", verifyRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
