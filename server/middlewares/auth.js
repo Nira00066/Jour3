040486a714e44756e2c4cb0ad2f80b4646ea5f83
Nira00066 <164557554+Nira00066@users.noreply.github.com>
2025-09-03 12:24:43 +0200

    modif

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,13 +1,17 @@
-// const jwt = require('jsonwebtoken')
-// require('doten').config();
+const jwt = require("jsonwebtoken");
+require("doten").config();
 
-// function validatorValidator(req,res,next){
-// const authHeader =req.headers.authorization;
-// if(!authHeader?.startWith("Bearer")){
-//     return res.status(401).json({message:"Token manquant ou mal formaté"});
-// }
-// const token = authHeader.split(" ")[1];
-// try{
-
-// }
-// }
+function validatorValidator(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader?.startWith("Bearer")) {
+    return res.status(401).json({ message: "Token manquant ou mal formaté" });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: "token invalid ou expiré" });
+  }
+}
