261c42262765e279b6e7f000e4665e43191f5857
Nira00066 <164557554+Nira00066@users.noreply.github.com>
2025-09-03 12:06:14 +0200

    git pull

---
+++ server/middlewares/auth.js
@@ -0,0 +1,13 @@
+// const jwt = require('jsonwebtoken')
+// require('doten').config();
+
+// function validatorValidator(req,res,next){
+// const authHeader =req.headers.authorization;
+// if(!authHeader?.startWith("Bearer")){
+//     return res.status(401).json({message:"Token manquant ou mal formaté"});
+// }
+// const token = authHeader.split(" ")[1];
+// try{
+
+// }
+// }
